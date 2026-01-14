9d1ce1e37ff9cb91cbcd1c34768cf10f57b11518
Suraj more <surajmorecombat@gmail.com>
2026-01-14 09:05:26 +0530

    changes

--- lib/src/modules/activity/activity.dart
+++ lib/src/modules/activity/activity.dart
@@ -6,6 +6,8 @@ import 'package:invoicediscounting/src/constant/app_color.dart';
 import 'package:invoicediscounting/src/mainlayout.dart';
 import 'package:invoicediscounting/src/modules/activity/market_news_detail.dart';
 import 'package:invoicediscounting/src/modules/activity/trainsation_all.dart';
+import 'package:invoicediscounting/src/modules/invest/invest_details.dart';
+import 'package:invoicediscounting/src/modules/portfolio/demat/demat_details.dart';
 import 'package:invoicediscounting/src/modules/profile/profile.dart';
 
 class Activity extends StatefulWidget {
@@ -17,7 +19,7 @@ class Activity extends StatefulWidget {
 
 class _ActivityState extends State<Activity> {
   int visibleCount = 2;
-  bool showTransaction = false;
+  bool showTransaction = true;
   @override
   Widget build(BuildContext context) {
     final bool isTablet = MediaQuery.of(context).size.width >= 600;
@@ -26,8 +28,7 @@ class _ActivityState extends State<Activity> {
       showDefaultBottom: true,
       ctx: 1,
 
-      appBar:
-   AppBar(
+      appBar: AppBar(
         elevation: 0,
         backgroundColor: backgroundColor,
         centerTitle: false,
@@ -45,8 +46,6 @@ class _ActivityState extends State<Activity> {
               radius: 34,
               child: Image.asset('assets/icons/profile.png'),
             ),
-
-  
           ),
         ),
         actions: [
@@ -94,8 +93,11 @@ class _ActivityState extends State<Activity> {
           // Notification Bell
           Padding(
             padding: const EdgeInsets.only(right: 10),
-            child: SvgPicture.asset('assets/icons/bell.svg',width: 20,height: 20,),
-      
+            child: SvgPicture.asset(
+              'assets/icons/bell.svg',
+              width: 20,
+              height: 20,
+            ),
           ),
         ],
       ),
@@ -103,19 +105,95 @@ class _ActivityState extends State<Activity> {
         child: Padding(
           padding: EdgeInsets.symmetric(
             horizontal: isTablet ? 120 : 20,
-          //  vertical: 16,
+            //  vertical: 16,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 10,),
-              totalEarningCard(context),
+              // totalEarningCard(context),
+               SizedBox(height: 10),
+              buildDemateAddCard(context),
+              SizedBox(height: 10),
+
+              Text(
+                'Recent Activity',
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyLarge?.copyWith(color: blackColor),
+              ),
+              SizedBox(height: 10),
+              _InvoiceCard(
+                buyer: 'Shristi Ispat',
+                seller: 'Flipkart',
+                imagepathbuyer: 'assets/images/imageone.png',
+                imagepatseller: 'assets/images/imagetwo.png',
+              ),
+       
               showTransactionCard(context),
-              WalletCard(),
+              // WalletCard(),
               // walletBalanceCard(context),
               marketNews(context),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildDemateAddCard(context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        // gradient: bannerCard,
+        color: onboardingTitleColor,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Update Your Demat Details',
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyMedium?.copyWith(color: Colors.white),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  'For secured and safe transaction with Birbalplus',
+                  style: Theme.of(
+                    context,
+                  ).textTheme.labelMedium?.copyWith(color: whiteColor),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 10),
+          ElevatedButton(
+            style: OutlinedButton.styleFrom(
+              backgroundColor: whiteColor,
+
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DematDetails()),
+              );
+            },
+            child: Text(
+              'Fill Details',
+              style: Theme.of(
+                context,
+              ).textTheme.labelLarge?.copyWith(color: blackColor),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -192,98 +270,6 @@ class _ActivityState extends State<Activity> {
       ),
     );
   }
-
-  // Widget walletBalanceCard(context) {
-  //   return Container(
-  //     margin: EdgeInsets.only(top: 20),
-  //     padding: const EdgeInsets.all(16),
-  //     decoration: BoxDecoration(
-  //       color: whiteColor,
-  //       borderRadius: BorderRadius.circular(12),
-  //       border: Border.all(color: Colors.grey.shade200),
-  //     ),
-  //     child: Column(
-  //       crossAxisAlignment: CrossAxisAlignment.start,
-  //       children: [
-  //         Text('Wallet Balance', style: Theme.of(context).textTheme.bodyLarge),
-  //         const SizedBox(height: 10),
-
-  //         Text(
-  //           '₹ 1,00,000 /-',
-  //           style: Theme.of(
-  //             context,
-  //           ).textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold),
-  //         ),
-
-  //         const SizedBox(height: 6),
-  //         Text(
-  //           'Add or Withdraw funds anytime',
-  //           style: Theme.of(context).textTheme.bodyMedium,
-  //         ),
-
-  //         const SizedBox(height: 18),
-
-  //         Row(
-  //           children: [
-  //             Expanded(
-  //               child: OutlinedButton(
-  //                 onPressed: () {
-  //                   // Navigator.push(
-  //                   //   context,
-  //                   //   MaterialPageRoute(
-  //                   //     builder: (context) => BankDemeteDetails(),
-  //                   //   ),
-  //                   // );
-  //                 },
-  //                 style: OutlinedButton.styleFrom(
-  //                   backgroundColor: onboardingTitleColor,
-  //                   //   side: BorderSide(color: onboardingTitleColor),
-  //                   shape: RoundedRectangleBorder(
-  //                     borderRadius: BorderRadius.circular(8),
-  //                   ),
-  //                   minimumSize: const Size(0, 48),
-  //                 ),
-  //                 child: Text(
-  //                   'Add',
-  //                   style: Theme.of(
-  //                     context,
-  //                   ).textTheme.labelLarge?.copyWith(color: whiteColor),
-  //                 ),
-  //               ),
-  //             ),
-  //             const SizedBox(width: 15),
-  //             Expanded(
-  //               child: ElevatedButton(
-  //                 onPressed: () {
-  //                   // Navigator.push(
-  //                   //   context,
-  //                   //   MaterialPageRoute(
-  //                   //     builder: (context) => DocumentVerified(),
-  //                   //   ),
-  //                   // );
-  //                 },
-  //                 style: ElevatedButton.styleFrom(
-  //                   side: BorderSide(color: onboardingTitleColor),
-  //                   backgroundColor: whiteColor,
-  //                   minimumSize: const Size(0, 48),
-  //                   shape: RoundedRectangleBorder(
-  //                     borderRadius: BorderRadius.circular(8),
-  //                   ),
-  //                 ),
-  //                 child: Text(
-  //                   'Withdraw',
-  //                   style: Theme.of(context).textTheme.labelLarge?.copyWith(
-  //                     color: onboardingTitleColor,
-  //                   ),
-  //                 ),
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 
   Widget marketNews(context) {
     final imageList = [
@@ -415,6 +401,150 @@ Interest rates remain stable''',
             ),
         ],
       ),
+    );
+  }
+}
+
+class _InvoiceCard extends StatelessWidget {
+  String imagepathbuyer;
+  String imagepatseller;
+
+  final String buyer, seller;
+  _InvoiceCard({
+    required this.imagepathbuyer,
+    required this.buyer,
+    required this.seller,
+    required this.imagepatseller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => InvestDetails()),
+        );
+      },
+      child: Card(
+        elevation: 0.1,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        // margin: const EdgeInsets.only(bottom: 16),
+        // padding: const EdgeInsets.all(16),
+        // decoration: BoxDecoration(
+        //   color: Colors.white,
+        //   borderRadius: BorderRadius.circular(16),
+        //   border: Border.all(color: Colors.grey.shade200),
+        // ),
+        child: Container(
+          //  margin: const EdgeInsets.only(bottom: 16),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: whiteColor,
+                    radius: 22,
+                    child: Image.asset(imagepathbuyer),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      buyer,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                  Icon(Icons.swap_horiz, size: 35, color: blackColor),
+                  Expanded(
+                    child: Text(
+                      seller,
+                      textAlign: TextAlign.end,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                  const SizedBox(width: 6),
+                  CircleAvatar(
+                    backgroundColor: whiteColor,
+                    radius: 22,
+                    child: Image.asset(imagepatseller),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  _Metric(label: 'Unit Cost', value: '₹1,00,000'),
+                  _Metric(label: 'XIRR', value: '13.65%', green: true),
+                  _Metric(label: 'Tenure', value: '90 Days'),
+                ],
+              ),
+              const SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const _Metric(label: 'Unit Left', value: '23/30'),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: onboardingTitleColor,
+                      shape: const StadiumBorder(),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => InvestDetails(),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'View Details',
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        SizedBox(width: 5),
+                        Icon(Icons.arrow_forward, size: 12),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Metric extends StatelessWidget {
+  final String label, value;
+  final bool green;
+  const _Metric({required this.label, required this.value, this.green = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: Theme.of(context).textTheme.bodySmall),
+        const SizedBox(height: 4),
+        Text(
+          value,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+            color: green ? Colors.green : Colors.black,
+          ),
+          // style: TextStyle(
+          //   fontWeight: FontWeight.bold,
+          //   color: green ? Colors.green : Colors.black,
+          // ),
+        ),
+      ],
     );
   }
 }
