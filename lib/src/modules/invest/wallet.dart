d546cfdc76ccc7cc8f5cc1133570a177ef3ae1ae
surajmorecombat <surajmorecombat@gmail.com>
2026-01-19 13:12:17 +0530

    changes done by android

--- lib/src/modules/invest/wallet.dart
+++ lib/src/modules/invest/wallet.dart
@@ -30,7 +30,6 @@ class _AddToWalletState extends State<AddToWallet> {
   TextEditingController unitController = TextEditingController();
   bool isEditingUnit = false;
 
-
   final FocusNode _unitFocusNode = FocusNode();
 
   @override
@@ -397,60 +396,73 @@ class _AddToWalletState extends State<AddToWallet> {
                           isEditingUnit
                               ? SizedBox(
                                 width: 60,
+                                height: 40,
                                 child: KeyboardActions(
                                   config: KeyboardActionsConfig(
-                        keyboardActionsPlatform: KeyboardActionsPlatform.IOS,
-                        actions: [
-                          KeyboardActionsItem(
-                            focusNode: _unitFocusNode,
-                            toolbarButtons: [
-                              (node) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(right: 12),
-                                  child: GestureDetector(
-                                    onTap: () => node.unfocus(),
-                                    child: Container(
-                                      padding: const EdgeInsets.symmetric(
-                                        horizontal: 18,
-                                        vertical: 8,
-                                      ),
-                                      decoration: BoxDecoration(
-                                        color:
-                                            onboardingTitleColor, // your brand color
-                                        borderRadius: BorderRadius.circular(20),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.black.withOpacity(
-                                              0.12,
-                                            ),
-                                            blurRadius: 6,
-                                            offset: const Offset(0, 2),
-                                          ),
+                                    keyboardActionsPlatform:
+                                        KeyboardActionsPlatform.IOS,
+                                    actions: [
+                                      KeyboardActionsItem(
+                                        focusNode: _unitFocusNode,
+                                        toolbarButtons: [
+                                          (node) {
+                                            return Padding(
+                                              padding: const EdgeInsets.only(
+                                                right: 12,
+                                              ),
+                                              child: GestureDetector(
+                                                onTap: () => node.unfocus(),
+                                                child: Container(
+                                                  padding:
+                                                      const EdgeInsets.symmetric(
+                                                        horizontal: 18,
+                                                        vertical: 8,
+                                                      ),
+                                                  decoration: BoxDecoration(
+                                                    color:
+                                                        onboardingTitleColor, // your brand color
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                          20,
+                                                        ),
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Colors.black
+                                                            .withOpacity(0.12),
+                                                        blurRadius: 6,
+                                                        offset: const Offset(
+                                                          0,
+                                                          2,
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                  child: const Text(
+                                                    "Done",
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.w600,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            );
+                                          },
                                         ],
                                       ),
-                                      child: const Text(
-                                        "Done",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
-                                    ),
+                                    ],
                                   ),
-                                );
-                              },
-                            ],
-                          ),
-                        ],
-                      ),
                                   child: TextField(
                                     controller: unitController,
                                     focusNode: _unitFocusNode,
                                     keyboardType: TextInputType.number,
                                     textAlign: TextAlign.center,
                                     style:
-                                        Theme.of(context).textTheme.displaySmall,
+                                        Theme.of(
+                                          context,
+                                        ).textTheme.displaySmall,
                                     decoration: const InputDecoration(
                                       isCollapsed: true,
                                       border: InputBorder.none,
